a4cb5ba9f24f5c8e897c2105b871f7caf49aa6e7
Shun Yamaguchi <shuny@Shuns-MacBook-Air.local>
2026-09-21 12:53:06 -0400

    modified: about the issue of unspecified conflicts

--- Sources/KDBXSync/Sync/SynchronizationError.swift
+++ Sources/KDBXSync/Sync/SynchronizationError.swift
@@ -1,4 +1,16 @@
-enum SynchronizationError: Error {
+import Foundation
+
+enum SynchronizationError: LocalizedError {
     case bothFilesMissing
     case conflict
+
+    var errorDescription: String? {
+        switch self {
+        case .bothFilesMissing:
+            return "Both the local and remote vault files are missing."
+
+        case .conflict:
+            return "The local and remote vault files both exist but contain different data. Neither file was modified."
+        }
+    }
 }
